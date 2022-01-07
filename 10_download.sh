d5a4d00663bdc227df1add0f5c868ab40e429e15
Anibal Franco <anibal.franco@extendeal.com>
2022-01-07 15:38:09 -0300

    fiveth commit - Se modifica apuntes, se agregan los script 09 y 10

---
+++ 10_download.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+#Autor: Anibal Franco
+#Version: 1.0
+#Programa para ejemplificar el uso de la descarga de informacion desde internet utilizando el comando wget
+
+echo "Descarga de Apache Tomcat desde Internet"
+echo -e "---------------\n"
+wget https://downloads.apache.org/tomcat/tomcat-10/v10.1.0-M8/bin/apache-tomcat-10.1.0-M8.zip
